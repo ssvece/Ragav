48732d6e7552e08045afd8a0310b7e2d7b2f8b84
Sakthivel Sugumar <ssv.ece@gmail.com>
2019-06-08 16:29:21 +0530

    First

--- Main.py
+++ Main.py
@@ -1,7 +1,9 @@
 from tkinter import *
 top=Tk()
-top.title("LnT")
+top.title("Lucifier V1.0")
 top.geometry("600x600")
-Label1 = Label(top, text= "CASTING BOM")
-Label1.pack()
+companyInfo=Label(top, text="LARSEN & TOUBRO RUBBER PROCESSING MACHINERY", font=("TIMES NEW ROMAN",15))
+Label1 = Button(top, text= "CASTING BOM")
+companyInfo.pack()
+Label1.place(x=270,y=300)
 top.mainloop()
