722be86989240c2754b8e3ef0794a334311558a1
Sakthivel Sugumar <ssv.ece@gmail.com>
2019-06-09 09:33:00 +0530

    main page completed

--- Main.py
+++ Main.py
@@ -1,9 +1,22 @@
+from PIL import ImageTk
+import PIL.Image
 from tkinter import *
-top=Tk()
-top.title("Lucifier V1.0")
-top.geometry("600x600")
-companyInfo=Label(top, text="LARSEN & TOUBRO RUBBER PROCESSING MACHINERY", font=("TIMES NEW ROMAN",15))
-Label1 = Button(top, text= "CASTING BOM")
-companyInfo.pack()
-Label1.place(x=270,y=300)
-top.mainloop()
+def onClickMechanicalBOM():
+    print("Mech BOM")
+
+def onClickElectricalBOM():
+    print ("Electrical BOM")
+
+root = Tk()
+root.title("LTRPM")
+root.geometry("600x600")
+companyName = Label(root, text="LARSEN & TOUBRO RUBBER PROCESSING MACHINERY", font=("TIMES NEW ROMAN", 15), fg="blue")
+mechanicalBomButton = Button(root, text="MECHANICAL BOM", command=onClickMechanicalBOM)
+mechanicalBomButton.place(x=100, y=300)
+electricalBomButton = Button(root, text="ELECTRICAL BOM", command=onClickElectricalBOM)
+electricalBomButton.place(x=375, y=300)
+companyImage = ImageTk.PhotoImage(PIL.Image.open("LNTRT.jpg"))
+companyLabel = Label(root, image=companyImage)
+companyLabel.place(x=130,y=100)
+companyName.pack()
+root.mainloop()
